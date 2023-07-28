7ba593b98308eb7c99319130776141da9b5f738e
Angel Salinas <85708983+AngelSalinasT@users.noreply.github.com>
2023-07-27 22:04:03 -0600

    Corrección de Menu responsive
    Corrección de Menu responsive

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,19 @@
 (function () {
     const openButton = document.querySelector('.nav__menu');
     const menu = document.querySelector('.nav__link');
-    const closemenu = document.querySelector('.nav__close');
+    const closeMenu = document.querySelector('.nav__close');
+    const closeOption = document.querySelectorAll('.nav__items');
 
     openButton.addEventListener('click', () => {
         menu.classList.add('nav__link--show');
 
     });
 
-    closemenu.addEventListener('click', () => {
+    closeMenu.addEventListener('click', () => {
+        menu.classList.remove('nav__link--show');
+    });
+
+    menu.addEventListener('click', () => {
         menu.classList.remove('nav__link--show');
     });
 })();
